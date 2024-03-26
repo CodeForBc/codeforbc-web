3656ebca0b4e65751620dccd5d5e3bd58175284f
CorreyL <correylim@gmail.com>
2024-03-25 17:02:14 -0700

    Change references from OpenBC to CodeForBC

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -10,29 +10,31 @@ export default function About() {
         About
       </Typography>
       <Typography variant="body2" paragraph>
-        OpenBC, a community-driven initiative dedicated to harnessing the power
-        of technology, collaboration, and innovation to create positive impacts
-        across British Columbia, Canada. Our mission is to bring together
-        individuals passionate about technology, civic engagement, and community
-        development to address the unique challenges and opportunities faced by
-        our province.
+        CodeForBC, a community-driven initiative dedicated to harnessing the
+        power of technology, collaboration, and innovation to create positive
+        impacts across British Columbia, Canada. Our mission is to bring
+        together individuals passionate about technology, civic engagement, and
+        community development to address the unique challenges and opportunities
+        faced by our province.
       </Typography>
       <Typography variant="body2" paragraph>
         <strong>
-          Utilize Your Skills for the Betterment of British Columbia with OpenBC
+          Utilize Your Skills for the Betterment of British Columbia with
+          CodeForBC
         </strong>
       </Typography>
       <Typography variant="body2" paragraph>
         Unlock the power of your skills and expertise to drive positive change
-        in British Columbia through OpenBC. At OpenBC, we&apos;re passionate
-        about leveraging your unique abilities for the advancement of our
-        province. Whether you&apos;re a tech enthusiast, a creative visionary,
-        or a community leader, your skills can play a crucial role in addressing
-        challenges and creating opportunities.
+        in British Columbia through CodeForBC. At CodeForBC, we&apos;re
+        passionate about leveraging your unique abilities for the advancement of
+        our province. Whether you&apos;re a tech enthusiast, a creative
+        visionary, or a community leader, your skills can play a crucial role in
+        addressing challenges and creating opportunities.
       </Typography>
       <Typography variant="body2">
         <strong>
-          Contribute to Open Source Projects for BC, from Anywhere, with OpenBC
+          Contribute to Open Source Projects for BC, from Anywhere, with
+          CodeForBC
         </strong>
       </Typography>
     </Container>
