31aa9150c461278922ceab10c5dd2763274157e6
CorreyL <correylim@gmail.com>
2024-04-05 20:13:05 -0700

    Remove basePath variable

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 
-const basePath = process.env.NODE_ENV === 'production' ? '' : '';
 const output = process.env.NODE_ENV === 'production' ? 'export' : 'standalone';
 const distDir = process.env.NODE_ENV === 'production' ? 'dist' : '.next';
 
 const nextConfig = {
-  basePath: basePath,
   output: output,
   distDir: distDir,
   images: {
