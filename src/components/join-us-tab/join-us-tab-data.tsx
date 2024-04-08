c0b0a5a0108ce6eb1f636b8b214fe70b9b4138cb
Ahmad Rashid Akhtar <ahmadrashidakhtar@gmail.com>
2024-04-07 18:05:17 -0700

    code refactoring

---
+++ src/components/join-us-tab/join-us-tab-data.tsx
@@ -0,0 +1,32 @@
+export const volunteerTabData = [
+  {
+    title: 'Tell Us About Yourself',
+    description:
+      'Fill out our 2 minute form so we can help match you with opportunities.',
+  },
+  {
+    title: 'Onboarding',
+    description:
+      'Our team will reach out and set up a first meeting to get to know each other.',
+  },
+  {
+    title: 'Have Questions?',
+    description:
+      'Find you answers in FAQ or email codeforbc@gmail.com with any questions or feedback.',
+  },
+];
+
+export const partnerTabData = [
+  {
+    icon_id: 'Make An Impact',
+    title: 'Make An Impact',
+  },
+  {
+    icon_id: 'Community',
+    title: 'Give Back To Your Community',
+  },
+  {
+    icon_id: 'fluent',
+    title: 'Attract Talents',
+  },
+];
--- src/components/join-us-tab/join-us-tab.tsx
+++ src/components/join-us-tab/join-us-tab.tsx
@@ -1,10 +1,11 @@
 import { TabContext, TabList, TabPanel } from '@mui/lab';
 import { Box, Button, Container, Tab, Typography } from '@mui/material';
 import { useState } from 'react';
+import { partnerTabData, volunteerTabData } from './join-us-tab-data';
 import './join-us-tab.scss';
 
 export default function JoinUsTab() {
-  const [value, setValue] = useState('1');
+  const [value, setValue] = useState('Volunteers');
   const handleChange = (event: React.SyntheticEvent, newValue: string) => {
     setValue(newValue);
   };
@@ -23,17 +24,17 @@ export default function JoinUsTab() {
                 <Tab
                   className="join-us-tab__button"
                   label="Volunteers"
-                  value="1"
+                  value="Volunteers"
                 />
                 <Tab
                   className="join-us-tab__button"
                   label="Become A Partner"
-                  value="2"
+                  value="Become A Partner"
                 />
               </TabList>
             </Box>
           </Box>
-          <TabPanel value="1">
+          <TabPanel value="Volunteers">
             <Box className="volunteer-tab">
               <Box className="volunteer-tab__heading-container">
                 <Typography className="volunteer-tab__heading" variant="h3">
@@ -46,52 +47,25 @@ export default function JoinUsTab() {
                 </Typography>
               </Box>
               <Box className="volunteer-tab__list-container">
-                <Box className="volunteer-tab__item">
-                  <Typography className="volunteer-tab__item-number">
-                    1
-                  </Typography>
-                  <Box className="volunteer-tab__item-text-container">
-                    <Typography className="volunteer-tab__item-heading">
-                      Tell Us About Yourself
+                {volunteerTabData.map((volunteerTabItem, index) => (
+                  <Box key={index} className="volunteer-tab__item">
+                    <Typography className="volunteer-tab__item-number">
+                      {index + 1}
                     </Typography>
-                    <Typography className="volunteer-tab__item-text">
-                      Fill out our 2 minute form so we can help match you with
-                      opportunities.
-                    </Typography>
+                    <Box className="volunteer-tab__item-text-container">
+                      <Typography className="volunteer-tab__item-heading">
+                        {volunteerTabItem.title}
+                      </Typography>
+                      <Typography className="volunteer-tab__item-text">
+                        {volunteerTabItem.description}
+                      </Typography>
+                    </Box>
                   </Box>
-                </Box>
-                <Box className="volunteer-tab__item">
-                  <Typography className="volunteer-tab__item-number">
-                    2
-                  </Typography>
-                  <Box className="volunteer-tab__item-text-container">
-                    <Typography className="volunteer-tab__item-heading">
-                      Onboarding
-                    </Typography>
-                    <Typography className="volunteer-tab__item-text">
-                      Our team will reach out and set up a first meeting to get
-                      to know each other.
-                    </Typography>
-                  </Box>
-                </Box>
-                <Box className="volunteer-tab__item">
-                  <Typography className="volunteer-tab__item-number">
-                    3
-                  </Typography>
-                  <Box className="volunteer-tab__item-text-container">
-                    <Typography className="volunteer-tab__item-heading">
-                      Have Questions?
-                    </Typography>
-                    <Typography className="volunteer-tab__item-text">
-                      Find you answers in FAQ or email codeforbc.ca@gmail.com
-                      with any questions or feedback.
-                    </Typography>
-                  </Box>
-                </Box>
+                ))}
               </Box>
             </Box>
           </TabPanel>
-          <TabPanel value="2">
+          <TabPanel value="Become A Partner">
             <Box className="partner-tab">
               <Box className="partner-tab__heading-container">
                 <Typography className="partner-tab__heading">
@@ -102,31 +76,22 @@ export default function JoinUsTab() {
                 </Typography>
               </Box>
               <Box className="partner-tab__item-container">
-                <Box className="partner-tab__item">
-                  <svg className="partner-tab__icon">
-                    <use xlinkHref="/assets/partner-sprite.svg#Make An Impact"></use>
-                  </svg>
+                {partnerTabData.map((partnerTabItem) => (
+                  <Box
+                    key={partnerTabItem.icon_id}
+                    className="partner-tab__item"
+                  >
+                    <svg className="partner-tab__icon">
+                      <use
+                        xlinkHref={`/assets/partner-sprite.svg#${partnerTabItem.icon_id}`}
+                      ></use>
+                    </svg>
 
-                  <Typography className="partner-tab__item-title">
-                    Make An Impact
-                  </Typography>
-                </Box>
-                <Box className="partner-tab__item">
-                  <svg className="partner-tab__icon">
-                    <use xlinkHref="/assets/partner-sprite.svg#Community"></use>
-                  </svg>
-                  <Typography className="partner-tab__item-title">
-                    Give Back To Your Community
-                  </Typography>
-                </Box>
-                <Box className="partner-tab__item">
-                  <svg className="partner-tab__icon">
-                    <use xlinkHref="/assets/partner-sprite.svg#fluent"></use>
-                  </svg>
-                  <Typography className="partner-tab__item-title">
-                    Attract Talents
-                  </Typography>
-                </Box>
+                    <Typography className="partner-tab__item-title">
+                      {partnerTabItem.title}
+                    </Typography>
+                  </Box>
+                ))}
               </Box>
               <Button
                 className="banner__button"
