3a02771da832f21f23163f498bd7ae7eb6ab63af
CorreyL <correylim@gmail.com>
2024-04-07 19:33:29 -0700

    Fix MUI CSS taking precedence in Production
    Fixes styling issues with the Footer rendering correctly in Development,
    but not Production, due to different ordering when a NextJS Production
    build is made

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import Footer from '@/components/footer/footer';
 import Header from '@/components/header/header';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
+import { StyledEngineProvider } from '@mui/material/styles';
 import { Lato } from 'next/font/google';
 import React from 'react';
 import './globals.scss';
@@ -15,9 +16,11 @@ export default function RootLayout({ children }: any) {
     <html lang="en">
       <body className={lato.className}>
         <Header />
-        <AppRouterCacheProvider options={{ enableCssLayer: true }}>
-          {children}
-        </AppRouterCacheProvider>
+        <StyledEngineProvider injectFirst>
+          <AppRouterCacheProvider options={{ enableCssLayer: true }}>
+            {children}
+          </AppRouterCacheProvider>
+        </StyledEngineProvider>
         <Footer />
       </body>
     </html>
