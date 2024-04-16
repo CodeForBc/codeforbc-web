4e06ac319d7ac96143d8e5a8787e915fa9a284e8
CorreyL <correylim@gmail.com>
2024-04-15 17:43:36 -0700

    Change order of header elements to match design

--- src/components/header/header-data.tsx
+++ src/components/header/header-data.tsx
@@ -2,8 +2,8 @@ import HeaderEnum from '../../enum/header-enum';
 
 const headerData = [
   { label: 'Our Projects', value: HeaderEnum.projects, href: '/projects' },
-  { label: 'Join Us', value: HeaderEnum.joinUs, href: '/join-us' },
   { label: 'Who We Are', value: HeaderEnum.about, href: '/about' },
+  { label: 'Join Us', value: HeaderEnum.joinUs, href: '/join-us' },
 ];
 
 export default headerData;
