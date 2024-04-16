f65db073b9718eaad9370c17f8c135a43247692d
CorreyL <correylim@gmail.com>
2024-04-15 17:43:08 -0700

    Change header labels to reflect design text

--- src/components/header/header-data.tsx
+++ src/components/header/header-data.tsx
@@ -1,9 +1,9 @@
 import HeaderEnum from '../../enum/header-enum';
 
 const headerData = [
-  { label: 'Projects', value: HeaderEnum.projects, href: '/projects' },
+  { label: 'Our Projects', value: HeaderEnum.projects, href: '/projects' },
   { label: 'Join Us', value: HeaderEnum.joinUs, href: '/join-us' },
-  { label: 'About', value: HeaderEnum.about, href: '/about' },
+  { label: 'Who We Are', value: HeaderEnum.about, href: '/about' },
 ];
 
 export default headerData;
