3012b4734c57d9513bce26e3d342db0904fe8d8d
CorreyL <correylim@gmail.com>
2024-04-17 17:30:40 -0700

    Render JoinUs client as a React component
    Rather than directly exporting the client component for JoinUs, make
    JoinUs a function that returns it as a React component

--- src/app/join-us/page.tsx
+++ src/app/join-us/page.tsx
@@ -1,9 +1,11 @@
 import { Metadata } from 'next';
-import JoinUs from './page.client';
+import JoinUsClient from './page.client';
 
 export const metadata: Metadata = {
   title: 'Join Us | CodeForBC',
   description: 'How to join the CodeForBC organization',
 };
 
-export default JoinUs;
+export default function JoinUs() {
+  return <JoinUsClient />;
+}
