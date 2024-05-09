9a72fdff8fc8ab0665255a882f3b8b4ff0f88dd8
Ahmad Rashid Akhtar <ahmadrashidakhtar@gmail.com>
2024-05-08 20:25:50 -0700

    schema added to FAQ

--- src/components/faq/faq.tsx
+++ src/components/faq/faq.tsx
@@ -1,26 +1,59 @@
 'use client';
 
 import { Box, Typography } from '@mui/material';
+import Head from 'next/head';
 import React from 'react';
 import QuestionAndAnswer from '../question-and-answer/question-and-answer';
 import questionAndAnswersData from './faq-data';
 import './faq.scss';
 
+interface FAQItem {
+  question: string;
+  answer: string;
+}
+
 export default function FrequentlyAskedQuestions() {
   return (
-    <Box className="faq">
-      <Typography variant="h2" className="faq__heading">
-        Frequently Asked Questions
-      </Typography>
-      <Box className="faq__list">
-        {questionAndAnswersData.map((questionAndAnswer) => (
-          <QuestionAndAnswer
-            key={questionAndAnswer.question}
-            question={questionAndAnswer.question}
-            answer={questionAndAnswer.answer}
-          />
-        ))}
+    <>
+      <Head>
+        <script type="application/ld+json">
+          {`
+            {
+              "@context": "https://schema.org",
+              "@type": "FAQPage",
+              "mainEntity": [
+                ${questionAndAnswersData
+                  .map(
+                    (questionAndAnswer: FAQItem) =>
+                      `{
+                    "@type": "Question",
+                    "name": "${questionAndAnswer.question}",
+                    "acceptedAnswer": {
+                      "@type": "Answer",
+                      "text": "${questionAndAnswer.answer}"
+                    }
+                  }`
+                  )
+                  .join(',')}
+              ]
+            }
+          `}
+        </script>
+      </Head>
+      <Box className="faq">
+        <Typography variant="h2" className="faq__heading">
+          Frequently Asked Questions
+        </Typography>
+        <Box className="faq__list">
+          {questionAndAnswersData.map((questionAndAnswer: FAQItem) => (
+            <QuestionAndAnswer
+              key={questionAndAnswer.question}
+              question={questionAndAnswer.question}
+              answer={questionAndAnswer.answer}
+            />
+          ))}
+        </Box>
       </Box>
-    </Box>
+    </>
   );
 }
