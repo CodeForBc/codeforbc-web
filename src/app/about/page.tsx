13de45333e59aec4ead6adeed4e3fcd6f8e69aa7
Ahmad Rashid Akhtar <ahmadrashidakhtar@gmail.com>
2024-05-13 18:29:25 -0700

    structured data about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,7 +3,7 @@ import { getTeamMemberData } from '@/utils/get-team-members-data/get-team-member
 import { Box, Button, Typography } from '@mui/material';
 import Container from '@mui/material/Container';
 import { Metadata } from 'next';
-import Head from 'next/head';
+import Script from 'next/script';
 import React from 'react';
 import './about.scss';
 
@@ -24,24 +24,26 @@ export const metadata: Metadata = {
 export default async function About() {
   const teamMemberData = await getTeamMemberData();
 
+  const teamMemberJson = teamMemberData.map((teamMember: TeamMember) => ({
+    '@context': 'https://schema.org',
+    '@type': 'Person',
+    name: teamMember.name,
+    jobTitle: teamMember.job_title,
+    description: teamMember.bio || '',
+    url: 'https://www.codeforbc.ca/about',
+    image: teamMember.profile_image_link
+      ? `https://www.codeforbc.ca${teamMember.profile_image_link}`
+      : '',
+    sameAs: [teamMember.linkedin_link, teamMember.github_link || ''],
+  }));
+
   return (
     <Container maxWidth="lg" className="about-page">
-      <Head>
-        {teamMemberData.map((teamMember: TeamMember, index: number) => (
-          <script type="application/ld+json" key={index}>
-            {`{
-              "@context": "https://schema.org",
-              "@type": "Person",
-              "name": "${teamMember.name}",
-              "jobTitle": "${teamMember.job_title}",
-              "description": "${teamMember.bio || ''}",
-              "url": "https://www.codeforbc.ca/about",
-              "image": "https://codeforbc-web.vercel.app${teamMember.profile_image_link || ''}",
-              "sameAs": ["${teamMember.linkedin_link}", "${teamMember.github_link || ''}"]
-            }`}
-          </script>
-        ))}
-      </Head>
+      <Script
+        id="jsonLD_team-member"
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(teamMemberJson) }}
+      />
       <Box className="about-page__heading-container">
         <Typography className="page-heading about-page__heading" variant="h1">
           Meet Our Team
