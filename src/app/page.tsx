8d468442ac8141564f62b9fa4a19459839d2c2df
Ahmad Akhtar <105146344+Unknown-0perator@users.noreply.github.com>
2024-05-15 15:49:18 -0700

    Update src/app/page.tsx
    Co-authored-by: Correy Lim <correylim@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,39 +12,35 @@ export default function Home() {
     <>
       <HomePage />
 
-      <Script
-        id="jsonLD_home-page"
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{
-          __html: JSON.stringify({
-            '@context': 'https://schema.org',
-            '@type': 'Organization',
-            name: 'CodeForBC',
-            legalName: 'Code For BC',
-            url: 'https://www.codeforbc.ca/',
-            logo: 'https://www.codeforbc.ca/assets/logo.webp',
-            foundingDate: '2024',
-            founders: [
-              {
-                '@type': 'Person',
-                name: 'Sam Huo',
-              },
-            ],
-            address: {
-              '@type': 'PostalAddress',
-              addressLocality: 'Vancouver',
-              addressRegion: 'Lower Mainland region of British Columbia',
-              addressCountry: 'CA',
+      <Script id="jsonLD_home-page" type="application/ld+json">
+        {JSON.stringify({
+          '@context': 'https://schema.org',
+          '@type': 'Organization',
+          name: 'CodeForBC',
+          legalName: 'Code For BC',
+          url: 'https://www.codeforbc.ca/',
+          logo: 'https://www.codeforbc.ca/assets/logo.webp',
+          foundingDate: '2024',
+          founders: [
+            {
+              '@type': 'Person',
+              name: 'Sam Huo',
             },
-            contactPoint: {
-              '@type': 'ContactPoint',
-              contactType: 'Info',
-              email: 'codeforbc@gmail.com',
-            },
-            sameAs: ['https://www.linkedin.com/company/code-for-bc/'],
-          }),
-        }}
-      />
+          ],
+          address: {
+            '@type': 'PostalAddress',
+            addressLocality: 'Vancouver',
+            addressRegion: 'Lower Mainland region of British Columbia',
+            addressCountry: 'CA',
+          },
+          contactPoint: {
+            '@type': 'ContactPoint',
+            contactType: 'Info',
+            email: 'codeforbc@gmail.com',
+          },
+          sameAs: ['https://www.linkedin.com/company/code-for-bc/'],
+        })}
+      </Script>
     </>
   );
 }
