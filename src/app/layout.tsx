5da0e5c704a0258e142784888aa0ac2ebb2420c7
CorreyL <correylim@gmail.com>
2024-05-21 16:40:54 -0700

    Switch Lato for Lexend
    Per Figma design

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,11 @@ import Footer from '@/components/footer/footer';
 import Header from '@/components/header/header';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
 import { StyledEngineProvider } from '@mui/material/styles';
-import { Lato } from 'next/font/google';
+import { Lexend } from 'next/font/google';
 import React from 'react';
 import './globals.scss';
 
-const lato = Lato({
+const lexend = Lexend({
   subsets: ['latin'],
   weight: ['100', '300', '400', '700', '900'],
 });
@@ -14,7 +14,7 @@ const lato = Lato({
 export default function RootLayout({ children }: any) {
   return (
     <html lang="en">
-      <body className={lato.className}>
+      <body className={lexend.className}>
         <Header />
         <StyledEngineProvider injectFirst>
           <AppRouterCacheProvider options={{ enableCssLayer: true }}>
