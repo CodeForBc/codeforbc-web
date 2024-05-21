e5b588a353aadf020eb4f5227b15bcb0886a3eae
CorreyL <correylim@gmail.com>
2024-05-21 16:12:43 -0700

    Add exclaimation mark to Join Us label
    Match Figma design

--- src/components/header/header-data.tsx
+++ src/components/header/header-data.tsx
@@ -1,7 +1,7 @@
 const headerData = [
   { label: 'Our Projects', href: '/projects' },
   { label: 'Who We Are', href: '/about' },
-  { label: 'Join Us', href: '/join-us' },
+  { label: 'Join Us!', href: '/join-us' },
 ];
 
 export default headerData;
