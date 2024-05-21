79b594b7cfddca5ff1f8b8f9dd31666d12011502
CorreyL <correylim@gmail.com>
2024-05-21 16:12:23 -0700

    Remove unused data

--- src/components/header/header-data.tsx
+++ src/components/header/header-data.tsx
@@ -1,9 +1,7 @@
-import HeaderEnum from '../../enum/header-enum';
-
 const headerData = [
-  { label: 'Our Projects', value: HeaderEnum.projects, href: '/projects' },
-  { label: 'Who We Are', value: HeaderEnum.about, href: '/about' },
-  { label: 'Join Us', value: HeaderEnum.joinUs, href: '/join-us' },
+  { label: 'Our Projects', href: '/projects' },
+  { label: 'Who We Are', href: '/about' },
+  { label: 'Join Us', href: '/join-us' },
 ];
 
 export default headerData;
--- src/enum/header-enum.tsx
+++
@@ -1,7 +0,0 @@
-const enum HeaderEnum {
-  projects = 1,
-  joinUs = 2,
-  about = 3,
-}
-
-export default HeaderEnum;
