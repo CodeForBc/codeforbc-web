b72f502ee5382d1407d8073dbd4f18331700324f
CorreyL <correylim@gmail.com>
2024-05-21 18:38:59 -0700

    Add and render simple component for Technologies

--- src/app/projects/[project_id]/page.tsx
+++ src/app/projects/[project_id]/page.tsx
@@ -1,6 +1,35 @@
+import { Link as LinkInterface } from '@/app/dataModels/link';
+import { Project as ProjectInterface } from '@/app/dataModels/project';
 import { getLocalProjectData } from '@/utils/get-local-project-data/get-local-project-data';
+import { IconButton } from '@mui/material';
+import Image from 'next/image';
 import { redirect } from 'next/navigation';
 import React from 'react';
+
+function Technologies(projectData: ProjectInterface) {
+  const { languages, links, tools } = projectData;
+
+  return (
+    <section>
+      <h1>Technologies Used</h1>
+      <h2>Languages</h2>
+      <p>{languages?.join(', ')}</p>
+      <h2>Tools</h2>
+      <p>{tools?.join(', ')}</p>
+      <h2>Links</h2>
+      {links.map((link: LinkInterface, idx: number) => (
+        <IconButton href={link.url} key={`project-link-${idx}`}>
+          <Image
+            src="/assets/github-mark.svg"
+            alt={link.title}
+            width={20}
+            height={20}
+          />
+        </IconButton>
+      ))}
+    </section>
+  );
+}
 
 export default async function Project({
   params,
@@ -15,5 +44,5 @@ export default async function Project({
     redirect('/projects');
   }
 
-  return <section>{params.project_id}</section>;
+  return <>{Technologies(projectData)}</>;
 }
