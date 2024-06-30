29afeb8ac07fe6cfad0f0dfb386664208d4812ee
Ahmad Rashid Akhtar <ahmadrashidakhtar@gmail.com>
2024-06-29 20:07:04 -0700

    mobile changed to tablet

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,66 +3,44 @@
 import headerData from '@/components/header/header-data';
 import CloseIcon from '@mui/icons-material/Close';
 import MenuIcon from '@mui/icons-material/Menu';
-import {
-  Box,
-  Container,
-  IconButton,
-  Menu,
-  MenuItem,
-  Link as MuiLink,
-} from '@mui/material';
-import { Lexend } from 'next/font/google';
+import { Box, Container, IconButton, Link as MuiLink } from '@mui/material';
 import Image from 'next/image';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import React, { useState } from 'react';
 import './header.scss';
 
-const lexend = Lexend({ subsets: ['latin'] });
-
 function Header() {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState<boolean>(false);
-  // const [mobileMenuAnchor, setMobileMenuAnchor] = useState<HTMLElement | null>(
-  //   null
-  // );
-  // const [menuClose, setMenuClose] = useState<boolean>(false);
-  // const mobileMenuOpen = Boolean(mobileMenuAnchor);
-  // const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-  //   setMobileMenuAnchor(event.currentTarget);
-  // };
+  const pathName = usePathname();
 
-  // const handleClose = () => {
-  //   setMobileMenuAnchor(null);
-  // };
+  const toggleMenu = () => setIsMobileMenuOpen((prev) => !prev);
 
   return (
     <Box className="header-container">
       <Container className="header">
         <Box className="header__icon-wrapper">
-          <MuiLink href="/" className="header__logo-container">
+          <MuiLink
+            href="/"
+            className="header__logo-container"
+            aria-label="Home"
+          >
             <Image
               src="/assets/codeforbc-logo.svg"
-              alt="Description"
+              alt="Code for BC Logo"
               width={84}
               height={76}
               priority={true}
               className="header__logo"
             />
           </MuiLink>
-          {isMobileMenuOpen ? (
-            <IconButton
-              onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
-              className="header__menu-icon"
-            >
-              <CloseIcon />
-            </IconButton>
-          ) : (
-            <IconButton
-              onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
-              className="header__menu-icon"
-            >
-              <MenuIcon />
-            </IconButton>
-          )}
+          <IconButton
+            onClick={toggleMenu}
+            className="header__menu-icon"
+            aria-label={isMobileMenuOpen ? 'Close Menu' : 'Open Menu'}
+          >
+            {isMobileMenuOpen ? <CloseIcon /> : <MenuIcon />}
+          </IconButton>
         </Box>
         <Box
           className={
@@ -74,7 +52,11 @@ function Header() {
           <ul className="header__navbar">
             {headerData.map((tab, index) => (
               <li key={`header-link-${index}`} className="header__navbar-item">
-                <Link className="header__navbar-link" href={tab.href}>
+                <Link
+                  className={`header__navbar-link ${pathName === tab.href ? 'header__navbar-link--active' : ''}`}
+                  href={tab.href}
+                  onClick={() => setIsMobileMenuOpen(false)}
+                >
                   {tab.label}
                 </Link>
               </li>
@@ -83,49 +65,6 @@ function Header() {
         </Box>
       </Container>
     </Box>
-
-    // <Box component={'header'} className="header-outer-box">
-    //   <MuiLink href="/" className="title-link">
-    //     <Image
-    //       src="/assets/codeforbc-logo.svg"
-    //       alt="Description"
-    //       width={84}
-    //       height={76}
-    //       priority={true}
-    //     />
-    //   </MuiLink>
-    //   <Box className="header-inner-box">
-    //     <ul className="nav__header">
-    //       {headerData.map((tab, index) => (
-    //         <li key={`header-link-${index}`} className="nav__list">
-    //           <Link href={tab.href}>{tab.label}</Link>
-    //         </li>
-    //       ))}
-    //     </ul>
-    //     <Box className="mobile-menu-btn">
-    //       <IconButton onClick={handleClick}>
-    //         {menuClose ? <MenuIcon /> : <CloseIcon />}
-    //       </IconButton>
-    //       <Menu
-    //         onClose={handleClose}
-    //         open={mobileMenuOpen}
-    //         anchorEl={mobileMenuAnchor}
-    //       >
-    //         {headerData.map((tab, index) => (
-    //           <MenuItem
-    //             onClick={handleClose}
-    //             key={`menu-item-${index}`}
-    //             className={`header-tab ${lexend.className}`}
-    //           >
-    //             <Link key={`menu-item-${index}`} href={tab.href}>
-    //               {tab.label}
-    //             </Link>
-    //           </MenuItem>
-    //         ))}
-    //       </Menu>
-    //     </Box>
-    //   </Box>
-    // </Box>
   );
 }
 
